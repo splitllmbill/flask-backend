b5f3eeb6ffc9d3dfddd5b7a3773707d757a3e86e
mojojomo-codes <krishna6122000@gmail.com>
2023-10-09 22:34:15 +0530

    Common db connection class

---
+++ models/database.py
@@ -0,0 +1,22 @@
+import pymongo
+
+class Database(object):
+    URI = "mongodb+srv://admin:urNZLHKvSpFVbvkm@cluster0.tcjo5lg.mongodb.net/?retryWrites=true&w=majority"
+    DB= None
+
+    @staticmethod
+    def initalize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DB = client['dummy']
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DB[collection].insert(data)
+    
+    @staticmethod
+    def find(collection, query):
+        return Database.DB[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DB[collection].find_one(query)
