4fb7cd8b0de9f2a0f15ff389762913e584daa8ec
sivaganesh193 <67482763+sivaganesh193@users.noreply.github.com>
2023-11-24 14:13:05 +0530

    Working LLM using PaLM API (#13)

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from pathlib import Path
 from dotenv import load_dotenv
 from routes.dbController import db_route
-# from routes.llmController import llm_route
+from routes.llmController import llm_route
 
 app = Flask(__name__)
 
@@ -14,10 +14,11 @@ app.config['DEBUG'] = os.getenv('DEBUG')
 app.config['DB_URL'] = os.getenv('DB_URL')
 app.config['DB_NAME'] = os.getenv('DB_NAME')
 app.config['UPLOADS_PATH'] = Path(os.getenv('UPLOADS_PATH'))
+app.config['LLM_API_KEY'] = os.getenv('LLM_API_KEY')
 
 # Configure Routes
 app.register_blueprint(db_route, url_prefix='/db')
-# app.register_blueprint(llm_route, url_prefix='/llm')
+app.register_blueprint(llm_route, url_prefix='/llm')
 
 if __name__ == '__main__':
     app.run(port=8081,debug=app.config['DEBUG'])
