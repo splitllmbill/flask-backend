de750cf2fecf9bd0458c6ac82f09bff20f5061a6
adi-codes <71934760+adi-codes@users.noreply.github.com>
2023-11-26 12:56:08 +0530

    event apis and response fix

--- resources/common.py
+++ resources/common.py
@@ -1,18 +1,21 @@
 
-from models.common import JSONSerializer
 
-class CreditorDetail(JSONSerializer):
+class CreditorDetail:
     def __init__(self, id, name,amount):
         self.id = id
         self.name = name
         self.amount = amount
 
-class EventDue(JSONSerializer):
+class EventDue:
     def __init__(self, id, debtor,creditorDetails):
         self.id = id
         self.debtor = debtor
         self.creditorDetails= creditorDetails
 
-class EventDueSummary(JSONSerializer):
+class EventDueSummary:
       def __init__(self,eventDues):
         self.eventDues= eventDues
+
+class ExpenseResponse:
+      def __init__(self,expenses):
+        self.expenses= expenses
--- services/eventService.py
+++ services/eventService.py
@@ -1,5 +1,8 @@
+import numpy
 from models.common import DatabaseManager,Event
+from datetime import datetime as dt
 from resources.common import CreditorDetail,EventDue,EventDueSummary
+from bson import ObjectId
 from services import expenseService,eventService,shareService,userService
 dbManager = DatabaseManager()
 dbManager.connect()
@@ -57,23 +60,76 @@ def getEventDues(event_id):
         for creditor in result[debtor]:
             creditorName = userNameMap[str(creditor)]
             amount=result[debtor][creditor]
-            creditorDetails.append(CreditorDetail(creditor,creditorName,amount))
-        eventDues.append(EventDue(debtor,debtorName,creditorDetails))
+            creditorDetails.append(CreditorDetail(creditor,creditorName,amount).__dict__)
+        eventDues.append(EventDue(debtor,debtorName,creditorDetails).__dict__)
     finalResult=EventDueSummary(eventDues)
 
     return finalResult
 
 
 def getEventDuesForUser(event_id,user_id):
-    result={"inDebtTo":{},"isOwed":{}}
-    eventDues=getEventDues(event_id)
+    result={"inDebtTo":[],"isOwed":[]}
+    eventDuesSummary=getEventDues(event_id)
+    print(eventDuesSummary.__dict__)
     userName=userService.getUserNameById(user_id)
-    for personInDebt in eventDues:
-        if personInDebt == user_id:
-           result["inDebtTo"]=eventDues[personInDebt]
+    for personInDebt in eventDuesSummary.eventDues:
+        if personInDebt["id"] == user_id:
+           result["inDebtTo"].append(personInDebt["creditorDetails"])
         else:
-            for owedPerson in eventDues[personInDebt]:
-                if owedPerson==user_id:
-                    result["isOwed"][personInDebt]= eventDues[personInDebt][owedPerson]
+            for owedPerson in personInDebt["creditorDetails"]:
+                if owedPerson["id"]==user_id:
+                    result["isOwed"].append({"id":personInDebt["id"],"name":personInDebt["debtor"],"amount":owedPerson["amount"]})
     return result
                
+def saveEvent(user_id,request_data):
+    new_event = Event(**request_data)
+    if "id" in request_data.keys():
+        print("noob:")
+        print(request_data)
+        event=getEventByID(request_data['id'])
+        print("noob:")
+        original_set = set(event.users)
+        modified_set = set(new_event.users)
+
+        # Find the elements that are in the original set but not in the modified set
+        removed_elements = original_set - modified_set
+
+        # Convert the result back to a list
+        removed_elements_list = list(removed_elements)
+        print("noob:",removed_elements_list)
+        for expense in event.expenses:
+            if expense.paidBy in removed_elements_list:
+                raise Exception("user present in expenses")
+            for share in expense.shares:
+                if share.userId in removed_elements_list:
+                    raise Exception("user present in shares")
+
+        new_event.updatedBy=ObjectId(user_id)
+        new_event.updatedAt= dt.utcnow()
+    else:
+        new_event.createdBy=ObjectId(user_id)
+        new_event.createdAt= dt.utcnow()
+    new_event.save()
+    return new_event
+
+def getEventByID(event_id):
+    query={
+        "id":event_id
+    }
+    event = dbManager.findOne(Event,query)
+    if event is None:
+        raise Exception(ValueError)
+    return event
+
+def deleteEvent(event_id):
+    query = {
+        "id": event_id
+    }
+    event = dbManager.findOne(Event,query)
+    if event is None:
+        return False
+    for expense in event.expenses:
+        dbManager.delete(expense)
+    dbManager.delete(event)
+    return 'Successfully Deleted Event'
+
