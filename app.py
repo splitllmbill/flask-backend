0efaaff2c632976601b15a71759bd85980908a9a
sivaganesh193 <sivaganeshb193@gmail.com>
2024-01-07 14:39:14 +0530

    Add app.py for local run

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from pathlib import Path
+from dotenv import load_dotenv
+from routes.dbController import db_route
+from routes.llmController import llm_route
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Load environment variables
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DEBUG'] = os.getenv('DEBUG')
+app.config['DB_URL'] = os.getenv('DB_URL')
+app.config['DB_NAME'] = os.getenv('DB_NAME')
+app.config['UPLOADS_PATH'] = Path(os.getenv('UPLOADS_PATH'))
+app.config['LLM_API_KEY'] = os.getenv('LLM_API_KEY')
+
+# Configure Routes
+app.register_blueprint(db_route, url_prefix='/db')
+app.register_blueprint(llm_route, url_prefix='/llm')
+
+if __name__ == '__main__':
+    app.run(port=8081,debug=app.config['DEBUG'])
