a3916001771051b116ec5675296959c1fdd3f0d2
sivaganesh193 <sivaganeshb193@gmail.com>
2024-01-07 13:19:50 +0530

    AutoDeployment Check

--- api/app.py
+++ api/app.py
@@ -7,7 +7,6 @@ from routes.llmController import llm_route
 from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app, origins=['https://slb-frontend.vercel.app/'])
 CORS(app)
 
 # Load environment variables
