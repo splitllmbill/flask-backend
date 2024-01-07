77c15f0203b24fc878e919c7909e450ddae9746f
sivaganesh193 <sivaganeshb193@gmail.com>
2024-01-07 12:36:51 +0530

    Fix CORS error

--- api/app.py
+++ api/app.py
@@ -7,7 +7,8 @@ from routes.llmController import llm_route
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['https://slb-frontend.vercel.app/'])
+# CORS(app, origins=['https://slb-frontend.vercel.app/'])
+CORS(app)
 
 # Load environment variables
 load_dotenv()
