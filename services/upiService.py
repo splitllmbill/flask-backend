ea09d5e585bd7a33d895a32d75b8cf8694573ddf
sivaganesh193 <sivaganeshb193@gmail.com>
2024-03-31 02:01:08 +0530

    UPI Link and QR Generation

---
+++ services/upiService.py
@@ -0,0 +1,46 @@
+from bson import ObjectId
+from models.common import DatabaseManager, Account
+
+import qrcode
+from io import BytesIO
+
+dbManager = DatabaseManager()
+dbManager.connect()
+
+def generateUPILink(userId, amount, note):
+    query={
+        "userId":ObjectId(userId)
+    }
+    account = dbManager.findOne(Account,query)
+    if account is None:
+        return False
+    upiId = account.upiId
+    baseLink = 'pay?pa=' + upiId + '&pn=User1&tn=' + note + '&am=' + str(amount) + '&cu=INR'
+    return {
+        'upiLink': baseLink
+    }
+
+def generateUPIQR(userId, amount, note):
+    query={
+        "userId":ObjectId(userId)
+    }
+    account = dbManager.findOne(Account,query)
+    if account is None:
+        return False
+    upiId = account.upiId
+    upiData = 'upi://pay?pa=' + upiId + '&pn=User1&tn=' + note + '&am=' + str(amount) + '&cu=INR'
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(upiData)
+    qr.make(fit=True)
+    # Create an in-memory bytes buffer to store the QR code image
+    qr_img_buffer = BytesIO()
+    qr.make_image(fill_color="black", back_color="white").save(qr_img_buffer)
+    qr_img_buffer.seek(0)
+    return qr_img_buffer
+    
+
